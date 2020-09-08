928d9e960c682311e3859aed3080a5c531468ab4
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-09-08 18:48:41 -0400

    sum root to leaf py

---
+++ sum_root_to_leaf.py
@@ -0,0 +1,15 @@
+def sumRootToLeaf(self, root: TreeNode) -> int:
+
+       def dfs(node):
+            if (not node):
+                return ''
+            if not node.left and not node.right:
+                return [str(node.val)]
+            return [str(node.val) + i for i in dfs(node.left)] + [str(node.val) + i for i in dfs(node.right)]
+
+        count = 0
+        result = dfs(root)
+        for i in range(len(result)):
+            count += int(result[i], 2)
+
+        return count
