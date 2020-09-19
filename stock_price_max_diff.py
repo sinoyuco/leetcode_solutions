fb84bc09767d03aaaa9b0a9f636b59a23785c7f3
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-09-18 20:40:25 -0400

    stock price max diff linear solution py

--- stock_price_max_diff.py
+++ stock_price_max_diff.py
@@ -1,12 +1,26 @@
 def maxDifference(px):
     # BRUTE FORCE
-    if len(px) < 2:
-        return -1
+    # if len(px) < 2:
+    #     return -1
 
-    maxdiff = 0
+    # maxdiff = 0
+    # for i in range(len(px)):
+    #     a = max(px[i:])
+    #     if (a - px[i]) > maxdiff:
+    #         maxdiff = (a-px[i])
+
+    # return -1 if maxdiff == 0 else maxdiff
+
+    mark = max(px)
+    p = -1
     for i in range(len(px)):
-        a = max(px[i:])
-        if (a - px[i]) > maxdiff:
-            maxdiff = (a-px[i])
+        if px[i] < mark:
+            mark = px[i]
+        elif px[i] - mark > p:
+            p = px[i] - mark
+    return -1 if p == 0 else p
 
-    return -1 if maxdiff == 0 else maxdiff
+stockp = [7,1,3,5,2]
+stocks = [6,2,1]
+print(maxDifference(stockp)) # 4
+print(maxDifference(stocks)) # -1
