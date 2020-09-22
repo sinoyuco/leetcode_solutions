ef59434010199c03bc001a637a837714e335f9a1
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-09-22 10:48:37 -0400

    car pooling py

---
+++ car_pooling.py
@@ -0,0 +1,25 @@
+def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+    maps = {}
+    end = max(i[2] for i in trips)
+    start = min(i[1] for i in trips)
+
+    for i in trips:
+        if i[1] in maps:
+            maps[i[1]] += i[0]
+        else:
+            maps[i[1]] = i[0]
+
+        if i[2] in maps:
+            maps[i[2]] -= i[0]
+        else:
+            maps[i[2]] = -i[0]
+
+    count = 0
+    
+    for i in range(start, end+1):
+        if i in maps:
+            count += maps[i]
+        if count > capacity:
+            return False
+
+    return True
