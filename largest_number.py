87ea16e686e538f8f5b844b5ba9c527e8d73b5f2
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-09-25 11:58:27 -0400

    largest number py optimal

--- largest_number.py
+++ largest_number.py
@@ -1,22 +1,28 @@
-def largestNumber(nums):
-    def helper(num1, num2):
-        str1 = num1+num2
-        str2 = num2+num1
-        return True if int(str1) < int(str2) else False
 
-    def digitSort(lst):
-        sorted = False
-        while not sorted:
-            sorted = True
-            for i in range(len(lst)-1):
-                if helper(str(lst[i]), str(lst[i+1])):
-                    lst[i], lst[i+1] = lst[i+1], lst[i]
-                    sorted = False
-        return lst
-        
+class Helper(str):
+    def __lt__(num1, num2):
+        return num1+num2 > num2+num1
     
-    a = digitSort(nums)
-    print(a)
-    if sum(nums) == 0:
-        return '0'
-    return ''.join([str(x) for x in a])
+class Solution:
+
+    def largestNumber(nums):
+
+        # def digitSort(lst):
+        #     sorted = False
+        #     while not sorted:
+        #         sorted = True
+        #         for i in range(len(lst)-1):
+        #             if helper(str(lst[i]), str(lst[i+1])):
+        #                 lst[i], lst[i+1] = lst[i+1], lst[i]
+        #                 sorted = False
+        #     return lst
+            
+        
+        # a = digitSort(nums)
+        # print(a)
+        # if sum(nums) == 0:
+        #     return '0'
+        # return ''.join([str(x) for x in a])
+        nums = sorted(map(str, nums), key=Helper)
+        return '0' if nums[0] == "0" else "".join(nums)
+
