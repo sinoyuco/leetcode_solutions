19550445c7cf12bb987d9221d9b643fa1509b129
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-09-25 11:50:04 -0400

    largest number py brute force

--- largest_number.py
+++ largest_number.py
@@ -1,14 +1,9 @@
 def largestNumber(nums):
     def helper(num1, num2):
-        i = 0
-        while i < min(len(num1), len(num2)):
-            if int(num2[i]) > int(num1[i]):
-                return True
-            elif int(num1[i]) > int(num2[i]):
-                return False
-            else:
-                i+=1
-    
+        str1 = num1+num2
+        str2 = num2+num1
+        return True if int(str1) < int(str2) else False
+
     def digitSort(lst):
         sorted = False
         while not sorted:
@@ -19,6 +14,9 @@ def largestNumber(nums):
                     sorted = False
         return lst
         
+    
     a = digitSort(nums)
     print(a)
+    if sum(nums) == 0:
+        return '0'
     return ''.join([str(x) for x in a])
