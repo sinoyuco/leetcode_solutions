ae44bce6133b5a8d27564ce2ecb9f718206502af
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-09-26 14:22:41 -0400

    rectangular game optimal solution py

--- rectangular_game.py
+++ rectangular_game.py
@@ -1,21 +1,29 @@
 def solve(steps):
-    maps = {}
-    max_val = 0
-    for i in steps:
-        digits = i.split(' ')
-        for j in range(1, int(digits[0])+1):
-            for k in range(1, int(digits[1])+1):
-                key = str(j)+str(k)
-                if key in maps:
-                    maps[key] += 1
-                else:
-                    maps[key] = 1
+    # maps = {}
+    # max_val = 0
+    # for i in steps:
+    #     digits = i.split(' ')
+    #     for j in range(1, int(digits[0])+1):
+    #         for k in range(1, int(digits[1])+1):
+    #             key = str(j)+str(k)
+    #             if key in maps:
+    #                 maps[key] += 1
+    #             else:
+    #                 maps[key] = 1
 
-                if maps[key] > max_val: max_val = maps[key]
+    #             if maps[key] > max_val: max_val = maps[key]
                 
 
-    a = list(maps.values())
-    return a.count(max_val)
+    # a = list(maps.values())
+    # return a.count(max_val)
+    x,y = [],[]
+    for i in steps:
+        b = i.split(' ')
+        x_val,y_val = int(b[0]), int(b[1])
+        x.append(x_val)
+        y.append(y_val)
+    return min(x) * min(y)
+
 
 
 ex_steps = ['2 3', '4 7', '5 1']
