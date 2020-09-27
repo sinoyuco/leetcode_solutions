f07dfb8c0189d31ddb205346712f8879f2f75d5f
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-09-27 18:40:51 -0400

    word crunch py linear solution

--- word_crunch.py
+++ word_crunch.py
@@ -7,14 +7,15 @@ def word_crunch(str):
     while i < len(str)-2:
         if str[i] == str[i+1] == str[i+2]:
             j = i
-            while str[j] == str[j+1]:
+            while (j+1)<len(str) and str[j] == str[j+1]:
                 j+=1
             str = str[:i]+str[(j+1):]
-            print(str,i)
+            i = max(0, i-1)
         else:
             i+=1
     return str
 
 
-print(word_crunch('klabbbaa'))
-        
+print(word_crunch('kabbbaa'))
+print(word_crunch('abbbaa'))
+print(word_crunch('abnnnnke'))
