f3da8ea79574394c9ae4fa49956f9fd6a3b03e5d
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-09-29 15:44:28 -0400

    has cycle with two pointers py

---
+++ has_cycle.py
@@ -0,0 +1,8 @@
+def has_cycle(head):
+    fast = slow = head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+        if fast == slow:
+            return True
+    return False
