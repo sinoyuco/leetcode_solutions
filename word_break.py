9b9de555928e3a6006cb227e3d3a8a6945b21f16
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-09-29 12:19:16 -0400

    word break dp py optimal

--- word_break.py
+++ word_break.py
@@ -1,17 +1,30 @@
 def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-      maps = {}
-       for i in wordDict:
-            maps[i] = 1
+    maps = {}
+    #    for i in wordDict:
+    #         maps[i] = 1
 
-        j = 1
-        while j <= len(s):
-            if s in maps:
-                return True
+    #     j = 1
+    #     while j <= len(s):
+    #         if s in maps:
+    #             return True
 
-            if s[0:j] in maps:
-                s = s[j:]
-                j = 1
+    #         if s[0:j] in maps:
+    #             s = s[j:]
+    #             j = 1
+    #         else:
+    #             j += 1
+
+    #     return True if s == '' else False
+    for j in wordDict:
+            if len(j) in dic:
+                maps[len(j)].append(j)
             else:
-                j += 1
-
-        return True if s == '' else False
+                maps[len(j)]=[j]
+        
+        dp=[1]+[0]*len(s)
+        for i in range(len(s)):
+            for k in dic:
+                if i-k+1>=0 and dp[i-k+1] and s[i-k+1:i+1] in dic[k]:
+                    dp[i+1]=1
+        print(dp)
+        return dp[-1]
