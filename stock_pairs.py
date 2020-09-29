a0616ae51a571f564ef07e22e358d43291046166
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-09-28 21:36:46 -0400

    stock pairs linear solution not working for edge cases py

--- stock_pairs.py
+++ stock_pairs.py
@@ -2,10 +2,18 @@ def stockPairs(stocksProfit, target):
     # Write your code here
     count = 0
     maps = {}
+    # for i in range(len(stocksProfit)):
+    #     for j in range(i, len(stocksProfit)):
+    #         if stocksProfit[i] + stocksProfit[j] == target and (str(stocksProfit[i])+str(stocksProfit[j])) not in maps:
+    #             count += 1
+    #             maps[(str(stocksProfit[i])+str(stocksProfit[j]))] = 1
+    #             maps[(str(stocksProfit[j])+str(stocksProfit[i]))] = 1
+    # return count
+
     for i in range(len(stocksProfit)):
-        for j in range(i, len(stocksProfit)):
-            if stocksProfit[i] + stocksProfit[j] == target and (str(stocksProfit[i])+str(stocksProfit[j])) not in maps:
-                count += 1
-                maps[(str(stocksProfit[i])+str(stocksProfit[j]))] = 1
-                maps[(str(stocksProfit[j])+str(stocksProfit[i]))] = 1
+        if (target-stocksProfit[i]) in maps and maps[(target-stocksProfit[i])] == 0:
+            maps[(target-stocksProfit[i])] = 1
+            count += 1
+        else:
+            maps[stocksProfit[i]] = 0
     return count
