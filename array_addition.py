64557c3031eff4402432f00bbf79d52348416076
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-10-01 21:00:24 -0400

    array addition recursive py

---
+++ array_addition.py
@@ -0,0 +1,17 @@
+def array_addition(arr):
+    arr.sort()
+    target = arr.pop()
+
+    def dfs(tar, nums):
+        if len(nums)==0:
+            return tar==0
+
+        num_to_check = nums[0]
+        nums = nums[1:]
+        
+        return dfs(tar, nums) or dfs(tar-num_to_check, nums)
+
+    return dfs(target, arr)
+
+
+
