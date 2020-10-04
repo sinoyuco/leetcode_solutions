7450259c1e3e1914165ba2442baba309e5d71e52
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-10-03 21:50:19 -0400

    k-diff pairs py

--- kdiff_pairs.py
+++ kdiff_pairs.py
@@ -3,7 +3,6 @@ import collections
 
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
-        count = 0
         cntr = collections.Counter(nums)
         if k == 0:
             return len([k for k, v in cntr.items() if v > 1])
