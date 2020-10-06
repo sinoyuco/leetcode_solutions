c6d50412b0e162f0961fda757666d1090463fc6a
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-10-06 14:35:58 -0400

    reorder log files py

--- reorder_log_files.py
+++ reorder_log_files.py
@@ -1,14 +1,15 @@
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
         i = count = j = 0
-        while i < len(logs) and j < len(logs):
+        while i <= len(logs) and j <= len(logs)-1:
             splitted = logs[i].split()
             if splitted[1].isalpha():
                 count+=1
                 a = logs[i]
                 del logs[i]
-                logs.append(a)
+                logs.insert(0,a)
                 i-=1
+                j-=1
             i+=1
             j+=1
-        return sorted(logs[:count], key=lambda x: x.split[1])+logs[count+1:]
+        return sorted(logs[:count], key=lambda x: x[5:])+logs[count:]
