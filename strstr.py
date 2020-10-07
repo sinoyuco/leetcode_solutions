0c447daaaef1197318812b01ecc835fb89a7eb17
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-10-07 15:33:38 -0400

    strstr py

---
+++ strstr.py
@@ -0,0 +1,9 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        n = len(needle)
+        if n == 0:
+            return 0
+        for i in range(len(haystack)-n+1):
+            if haystack[i:i+n] == needle:
+                return i
+        return -1
