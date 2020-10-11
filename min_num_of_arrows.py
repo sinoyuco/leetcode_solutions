3860ad786f056d4823baf7819b917b91781d76ac
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-10-10 20:13:14 -0400

    min num of arrows

---
+++ min_num_of_arrows.py
@@ -0,0 +1,16 @@
+import math
+
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        p = sorted(points)
+        count = 0
+        current = [-math.inf]*2
+
+        for i in range(len(p)):
+            if p[i][0] > current[1]:
+                current = p[i]
+                count += 1
+            else:
+                current[1] = min(current[1], p[i][1])
+        return count
