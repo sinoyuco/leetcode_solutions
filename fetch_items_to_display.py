cf008b83572baa2b9ff5f806a505bdb833579cbb
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-10-13 10:51:28 -0400

    items to display py

--- fetch_items_to_display.py
+++ fetch_items_to_display.py
@@ -2,7 +2,7 @@ def fetchItemsToDisplay(items, sortParameter, sortOrder, itemsPerPage, pageNumbe
     # Write your code here
     rev = False if sortOrder == 0 else True
     #sort the array based on sort parameter and order
-    news = sorted(items, key=lambda x: x[sortParameter], reverse=rev)
+    news = sorted(items, key=lambda x: int(x[sortParameter]) if sortParameter != 0 else x[sortParameter], reverse=rev)
 
     result = []
     #append the name to result if item index divided by items per page indicates that
