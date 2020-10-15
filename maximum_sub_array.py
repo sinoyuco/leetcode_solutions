79ee019a77e062d4c9599343c7d6048804d675e4
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-10-14 21:53:42 -0400

    kadene with better space complexity

--- maximum_sub_array.py
+++ maximum_sub_array.py
@@ -3,16 +3,9 @@ import math
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
-        if len(nums) == 1:
-            return nums[0]
-
-        dp = [0]*len(nums)
-        dp[0] = nums[0]
-
-        maxval = nums[0]
-        for i in range(1, len(nums)):
-            dp[i] = max(nums[i], nums[i]+dp[i-1])
-            maxval = max(dp[i], maxval)
-        return maxval
+        max_val,s = -math.inf, 0
+        for i in nums:
+            s+=i
+            max_val = max(max_val, s)
+            s = max(s,0)
+        return max_val
