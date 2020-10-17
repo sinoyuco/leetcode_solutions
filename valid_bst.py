2145fab954abb4c4416e7f7313b38ae8b3450131
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-10-17 14:28:19 -0400

    valid bst recursive py

--- valid_bst.py
+++ valid_bst.py
@@ -1,13 +1,30 @@
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
 
-        arr = []
+        # arr = []
 
-        def dfs(node):
+        # def dfs(node):
+        #     if not node:
+        #         return
+        #     dfs(node.left)
+        #     arr.append(node.val)
+        #     dfs(node.right)
+        # dfs(root)
+        # for i in range(len(arr)-1):
+        #     if arr[i]>arr[i+1]:
+        #         return False
+        # return True
+
+        def dfs(node, lower, upper):
             if not node:
-                return
-            dfs(node.left)
-            arr.append(node.val)
-            dfs(node.right)
-        dfs(root)
-        return arr == sorted(list(set(arr)))
+                return True
+            if node.val <= lower or node.val >= upper:
+                return False
+
+            if not dfs(node.right, node.val, upper):
+                return False
+            if not dfs(node.left, lower, node.val):
+                return False
+            return True
+
+        return dfs(root, -math.inf, math.inf)
--- word_crunch.py
+++ word_crunch.py
@@ -10,7 +10,11 @@ def word_crunch(str):
             while (j+1)<len(str) and str[j] == str[j+1]:
                 j+=1
             str = str[:i]+str[(j+1):]
-            i = max(0, i-1)
+
+            if i-2 >= 0 and str[i-2]==str[i-1]:
+                i = max(0, i-2)
+            else:
+                i = max(0, i-1) 
         else:
             i+=1
     return str
@@ -19,3 +23,5 @@ def word_crunch(str):
 print(word_crunch('kabbbaa'))
 print(word_crunch('abbbaa'))
 print(word_crunch('abnnnnke'))
+print(word_crunch('deeedbbcccbdaa'))
+
