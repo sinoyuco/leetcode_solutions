c2f112448e4a298acfb542ad1bf33f6147506626
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-11-07 21:01:07 -0500

    first common ancestor node py

---
+++ tree/first_common_ancestor.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        res = None
+
+        def dfs(node):
+            if not node:
+                return False
+            l, r = dfs(node.left), dfs(node.right)
+            m = (node == p) or (node == q)
+            if m+l+r >= 2:
+                nonlocal res
+                res = node
+            return m or l or r
+        dfs(root)
+        return res
