fe72438d0fd7a6abfdc426975138a678821cdf09
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-11-07 20:01:44 -0500

    root successor node py

---
+++ tree/successor.py
@@ -0,0 +1,16 @@
+def findSuccessorNode(root):
+    if not root:
+        return None
+    if root.right:
+        r = root.right
+        while r.left:
+            r = r.left
+        return r
+    else:
+        a = root
+        par = a.parent
+        while par and par.left != a:
+            a = par
+            par = par.parent
+        return par
+        
