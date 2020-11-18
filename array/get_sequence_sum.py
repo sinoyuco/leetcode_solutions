c719f780ae7a6425b43dbb867d4e8e77e181d14c
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-11-18 14:21:00 -0500

    get sequence sum

---
+++ array/get_sequence_sum.py
@@ -0,0 +1,13 @@
+def getSequenceSum(i, j, k):
+    s = 0
+    if i > k:
+        for x in range(k, i):
+            s += x
+        for y in range(i, j+1):
+            s += 2*y
+    else:
+        for x in range(i, k):
+            s += x
+        for y in range(k, j+1):
+            s += 2*y
+    return s-j
