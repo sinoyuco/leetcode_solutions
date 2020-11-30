1ade6dd3318f3da398185a36202f16b58041414d
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-11-30 11:10:53 -0500

    build tree from inorder and preorder py

---
+++ tree/tree_from_inorder_preorder.py
@@ -0,0 +1,17 @@
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if len(preorder) > 0:
+            root = TreeNode(preorder[0])
+            idx = inorder.index(preorder[0])
+
+            lin, rin = inorder[:idx], inorder[(idx+1):]
+            lpre = preorder[1:(1+len(lin))]
+            rpre = preorder[(1+len(lin)):]
+
+            l = self.buildTree(lpre, lin)
+            r = self.buildTree(rpre, rin)
+
+            root.left, root.right = l, r
+
+            return root
+        return None
