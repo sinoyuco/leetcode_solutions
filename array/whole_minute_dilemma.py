0f04835cbe788f06bfe35f46688a95cdff8a685f
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-12-08 10:56:45 -0500

    whole minute dilemma linear with hashmap py

--- array/whole_minute_dilemma.py
+++ array/whole_minute_dilemma.py
@@ -1,9 +1,23 @@
+import collections
 def playlist(songs):
     # Write your code here
     # quadratic, how to make linear?
+    # ct = 0
+    # for i in range(len(songs)):
+    #     for j in range(i+1, len(songs)):
+    #         if not (songs[i]+songs[j]) % 60:
+    #             ct += 1
+    # return ct
+
+    #hashmap - linear
     ct = 0
-    for i in range(len(songs)):
-        for j in range(i+1, len(songs)):
-            if not (songs[i]+songs[j]) % 60:
-                ct += 1
+    maps = collections.defaultdict(int)
+    for i in songs:
+        if i % 60 == 0:
+            ct+=maps[0]
+        else:
+            ct += maps[60-i%60]
+        maps[i%60] += 1
     return ct
+
+
