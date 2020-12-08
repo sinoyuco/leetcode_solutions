6bfe13efd05dbc90dcf460ba6b8bf0b2009bf316
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-12-08 13:31:06 -0500

    co prime count py

--- array/co_prime_count.py
+++ array/co_prime_count.py
@@ -1,5 +1,17 @@
+import math
 def coprimeCount(A):
     # Write your code h
     res = []
-    return res
     for num in A:
+        count = 0
+        for i in range(1, num//2+1):
+            if i == 1 or math.gcd(i, num) == 1:
+                count+=2
+        res.append(count)
+    return res
+
+
+
+print(coprimeCount([5,8,14]))
+# [4,4,6]
+# [1,2,3,4], []
