ca72a3ee36570d7f3e8a2a23b3ef6f09b0ad6620
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-12-15 06:26:12 +0300

    majority element py

---
+++ array/majority_element.py
@@ -0,0 +1,10 @@
+import collections
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        maps = collections.defaultdict(int)
+        for i in nums:
+            maps[i] += 1
+            if maps[i] > len(nums)/2:
+                return i
