f620c61fa749d3119ef5200d4faa245ad3729eea
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-12-15 23:12:32 +0300

    decode ways py

---
+++ dp/decode_ways.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = [0] * (len(s)+1)
+        dp[0] = 1
+        dp[1] = 0 if s[0] == '0' else 1
+        for i in range(2, len(s)+1):
+            one = int(s[i-1:i])
+            two = int(s[i-2:i])
+            if one >= 1:
+                dp[i] += dp[i-1]
+            if 10 <= two <= 26:
+                dp[i] += dp[i-2]
+        return dp[-1]
