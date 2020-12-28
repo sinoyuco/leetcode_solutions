53c6775f981aa4df8d5e860130683e59fb552de6
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-12-28 19:06:46 +0300

    reach a number py

---
+++ array/reach_a_number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        if target == 0:
+            return 0
+        if target == 1:
+            return 1
+        target = abs(target)
+        tot = step = 0
+        while True:
+            if tot < target or (target-tot)%2 == 1:
+                step+=1
+                tot+=step
+            else:
+                return step
