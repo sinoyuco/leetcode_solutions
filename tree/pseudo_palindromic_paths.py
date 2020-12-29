e39c237c3b5d315998901e4b3ccb1a82d03ff516
Sinan Yucesan <sinan.yucesan@gmail.com>
2020-12-29 18:31:00 +0300

    pseudo palindromic paths py

---
+++ tree/pseudo_palindromic_paths.py
@@ -0,0 +1,28 @@
+import collections
+class Solution:
+    def palindrome(self, array):
+        maps = collections.Counter(array)
+        found = False
+        for k, v in maps.items():
+            if v % 2 == 1:
+                if found:
+                    return False
+                else:
+                    found = True
+        return True
+
+    def pseudoPalindromicPaths(self, root: TreeNode) -> int:
+
+        self.res = 0
+        def dfs(node, arr):
+            if not node:
+                return
+            if not node.left and not node.right:
+                print(arr+[node.val])
+                if self.palindrome(arr+[node.val]):
+                    self.res += 1
+            dfs(node.left, arr + [node.val])
+            dfs(node.right, arr + [node.val])
+
+        dfs(root, [])
+        return self.res
