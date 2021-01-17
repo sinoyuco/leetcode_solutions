ea6971dd3bec217af156f9701eccb01bd2512015
Sinan Yucesan <sinan.yucesan@gmail.com>
2021-01-17 15:00:59 -0500

    count vowel strings py

---
+++ array/count_vowel_strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countVowelStrings(self, n: int) -> int:
+        dp = [1]*5
+        for i in range(1, n):
+            for j in range(1, 5):
+                dp[j] += dp[j-1]
+        print(dp)
+        return sum(dp)
