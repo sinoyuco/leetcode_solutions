c5c955fc95e37552e8cea0c047d596740df5cee1
Sinan Yucesan <sinan.yucesan@gmail.com>
2021-01-19 12:58:37 -0500

    all full trees py

---
+++ tree/all_full_trees.py
@@ -0,0 +1,18 @@
+class Solution:
+    def allPossibleFBT(self, N: int) -> List[TreeNode]:
+        self.memo = {0: [], 1: [TreeNode(0)]}
+
+        def dfs(num):
+            if num not in self.memo:
+                res = []
+                for i in range(num):
+                    j = num-(i+1)
+                    for left in dfs(i):
+                        for right in dfs(j):
+                            c = TreeNode(0)
+                            c.left, c.right = left, right
+                            res.append(c)
+                self.memo[num] = res
+            return self.memo[num]
+
+        return dfs(N)
