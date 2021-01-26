1a3037366c08faa44ac3b18328f1fd9419f0b912
Sinan Yucesan <sinan.yucesan@gmail.com>
2021-01-26 09:38:14 -0500

    bottom left val bfs py

--- tree/bottom_left_val.py
+++ tree/bottom_left_val.py
@@ -2,17 +2,35 @@ import collections
 class Solution:
     def findBottomLeftValue(self, root: TreeNode) -> int:
 
-        self.maxval = -math.inf
-        self.res = -1
+        if not root:
+            return None
 
-        def dfs(node, d):
-            if not node:
-                return
-            if d > self.maxval:
-                self.maxval = d
-                self.res = node.val
-            dfs(node.left, d+1)
-            dfs(node.right, d+1)
+        queue = [root]
+        res = []
+        while queue:
+            l, t = len(queue), []
+            while l > 0:
+                c = queue.pop(0)
+                if c.left:
+                    queue.append(c.left)
+                if c.right:
+                    queue.append(c.right)
+                t.append(c.val)
+                l -= 1
+            res.append(t)
+        return res[-1][0]
 
-        dfs(root, 0)
-        return self.res
+        # self.maxval = -math.inf
+        # self.res = -1
+
+        # def dfs(node, d):
+        #     if not node:
+        #         return
+        #     if d > self.maxval:
+        #         self.maxval = d
+        #         self.res = node.val
+        #     dfs(node.left, d+1)
+        #     dfs(node.right, d+1)
+
+        # dfs(root, 0)
+        # return self.res
